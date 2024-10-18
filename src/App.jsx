26cfa391dcb80252bbfd0d6a346b47239b70eb98
sjake12 <apostol.sjake12@gmail.com>
2024-10-18 15:40:06 +0800

    added ui from shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
   const [message, setMessage] = useState("");
@@ -24,6 +25,7 @@ function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <Toaster />
     </>
   );
 }
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Button } from "../components/ui/button";
+import LoginForm from "@/components/LoginForm";
 
 export default function Login() {
   const [username, setUsername] = useState("");
@@ -25,69 +25,9 @@ export default function Login() {
     }
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setMessage("");
-
-    try {
-      const formData = new FormData();
-      formData.append("username", username);
-      formData.append("password", password);
-
-      const response = await axios.post("http://localhost/api/auth", formData);
-      const { permissions, role, token } = response.data;
-
-      // Store the token in localStorage
-      localStorage.setItem("token", token);
-
-      // Set the default Authorization header for future requests
-      // axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-
-      //Login the user
-      login({ username, role, permissions });
-
-      if (role === 1) {
-        navigate("/admin");
-      }
-
-      if (role === 2) {
-        navigate("/student");
-      }
-
-      console.log(response);
-    } catch (e) {
-      console.error(`There was an error! ${e}`);
-      setMessage(e.message);
-    }
-  };
-
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center bg-green-200">
-      <div className="flex flex-col gap-4 w-max h-max rounded-lg shadow-xl bg-white p-6">
-        <h1 className="text-4xl font-bold">Log In</h1>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-6 ">
-          <input
-            type="text"
-            placeholder="Username"
-            className="border border-black rounded-sm p-2"
-            name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="border border-black rounded-sm p-2"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          {message && <p className="text-red-500">User not found!</p>}
-          <Button type="submit">Log In</Button>
-        </form>
-      </div>
+    <div className="flex justify-center items-center h-screen">
+      <LoginForm />
     </div>
   );
 }
