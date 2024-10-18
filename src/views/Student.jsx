4f885f42487b66a9f2063feae47610f658e6585f
sjake12 <apostol.sjake12@gmail.com>
2024-10-18 23:17:31 +0800

    added a simple navbar for student

--- src/views/Student.jsx
+++ src/views/Student.jsx
@@ -2,6 +2,16 @@ import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Student() {
   const navigate = useNavigate();
@@ -37,12 +47,45 @@ export default function Student() {
 
   return (
     <>
-      <h1>
-        {firstname} {lastname} {course}
-      </h1>
-      <form onSubmit={handleSubmit}>
-        <Button type="submit">Logout</Button>
-      </form>
+      <div className="flex justify-between items-center w-full py-2 px-10 shadow-md">
+        <div className="flex gap-2 items-center">
+          <div className="flex-col">
+            <h1 className="scroll-m-20 text-xl font-semibold tracking-tight">
+              {firstname} {lastname}, {course}
+            </h1>
+            <Badge>Student</Badge>
+          </div>
+        </div>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="overflow-hidden rounded-full"
+            >
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <form onSubmit={handleSubmit}>
+                <Button variant="ghost" type="submit" size="sm">
+                  Logout
+                </Button>
+              </form>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </>
   );
 }
