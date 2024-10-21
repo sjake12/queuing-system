7de434b7b171ee9b996b8ac856d248d48dea36cd
sjake12 <apostol.sjake12@gmail.com>
2024-10-21 20:30:06 +0800

    made a component for the sidebar and added sidebarcomponent to student and admin

---
+++ src/components/SidebarComponent.jsx
@@ -0,0 +1,243 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuGroup,
+} from "@/components/ui/dropdown-menu";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarProvider,
+  SidebarRail,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import {
+  ChevronsUpDown,
+  ChevronRight,
+  LogOut,
+  CreditCard,
+  Settings,
+  FileInput,
+  Calendar,
+} from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from "@/components/ui/collapsible";
+import { DashboardIcon } from "@radix-ui/react-icons";
+import { Link } from "react-router-dom";
+import sksu from "@/assets/sksu1.png";
+
+export default function SidebarComponent({
+  role,
+  firstname,
+  lastname,
+  course,
+  isSmallScreen,
+  logOut,
+  group,
+}) {
+  return (
+    <SidebarProvider>
+      <Sidebar collapsible="icon">
+        <SidebarHeader>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                size="lg"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              >
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <Avatar>
+                    <AvatarImage src={sksu} />
+                    <AvatarFallback>SK</AvatarFallback>
+                  </Avatar>
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">
+                    Sultan Kudarat <br /> State University
+                  </span>
+                </div>
+                <Badge>{role}</Badge>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarMenu>
+            <SidebarMenuItem className="px-2 mt-4">
+              <SidebarMenuButton tooltip="Dashboard">
+                <DashboardIcon />
+                <a href="#">
+                  <span>Dashboard</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+          <SidebarGroup className="mt-2">
+            <SidebarGroupLabel>View</SidebarGroupLabel>
+            <SidebarMenu>
+              {group.navMain.map((item) => (
+                <Collapsible
+                  key={item.title}
+                  asChild
+                  defaultOpen={item.isActive}
+                  className="group/collapsible"
+                >
+                  <SidebarMenuItem>
+                    <CollapsibleTrigger asChild>
+                      <SidebarMenuButton tooltip={item.title}>
+                        {item.icon && <item.icon />}
+                        <span>{item.title}</span>
+                        <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                      </SidebarMenuButton>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {item.items.map((subItem) => (
+                          <SidebarMenuSubItem key={subItem.title}>
+                            <SidebarMenuSubButton asChild>
+                              <a href={subItem.url}>
+                                <span>{subItem.title}</span>
+                              </a>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  </SidebarMenuItem>
+                </Collapsible>
+              ))}
+            </SidebarMenu>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton
+                    size="lg"
+                    className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                  >
+                    <Avatar>
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <div className="grid flex-1 text-left text-sm leading-tight">
+                      <span className="truncate font-semibold">
+                        {firstname} {lastname}
+                      </span>
+                      <span className="truncate text-xs">{course}</span>
+                    </div>
+                    <ChevronsUpDown className="ml-auto size-4" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                  side={isSmallScreen ? "top" : "right"}
+                  align="end"
+                  sideOffset={4}
+                >
+                  <DropdownMenuLabel className="p-0 font-normal">
+                    <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                      <Avatar className="h-8 w-8 rounded-lg">
+                        <AvatarImage
+                          src="https://github.com/shadcn.png"
+                          alt="avatar"
+                        />
+                        <AvatarFallback className="rounded-lg">
+                          CN
+                        </AvatarFallback>
+                      </Avatar>
+                      <div className="grid flex-1 text-left text-sm leading-tight">
+                        <span className="truncate font-semibold">
+                          {firstname} {lastname}
+                        </span>
+                        <span className="truncate text-xs">{course}</span>
+                      </div>
+                    </div>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      <Settings />
+                      Settings
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <CreditCard />
+                      Billing
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem onClick={logOut}>
+                    <LogOut />
+                    Log out
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+        <SidebarRail />
+      </Sidebar>
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink asChild>
+                    <Link to="#">Building Your Application</Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+          </div>
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
--- src/views/Admin.jsx
+++ src/views/Admin.jsx
@@ -1,21 +1,105 @@
-import { Button } from "@/components/ui/button";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import SidebarComponent from "@/components/SidebarComponent";
 
 export default function Admin() {
   const navigate = useNavigate();
+  const [username, setUsername] = useState(localStorage.getItem("username"));
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [course, setCourse] = useState("");
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost/api/user?studentId=${username}`
+        );
+
+        setFirstname(response.data[0].firstname);
+        setLastname(response.data[0].lastname);
+        setCourse(response.data[0].course);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    loadUser();
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 500);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Call once to set initial state
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const logOut = (e) => {
     e.preventDefault();
 
     localStorage.clear();
     navigate("/");
   };
 
+  const group = {
+    navMain: [
+      {
+        title: "Clearance",
+        url: "#",
+        icon: "FileInput",
+        isActive: true,
+        items: [
+          {
+            title: "Queue",
+            url: "#",
+          },
+          {
+            title: "Requirements",
+            url: "#",
+          },
+          {
+            title: "Payments",
+            url: "#",
+          },
+        ],
+      },
+      {
+        title: "Events",
+        url: "#",
+        icon: "Calendar",
+        items: [
+          {
+            title: "Upcoming",
+            url: "#",
+          },
+          {
+            title: "Listed",
+            url: "#",
+          },
+        ],
+      },
+    ],
+  };
+
   return (
-    <Button type="submit" onClick={handleSubmit}>
-      Logout
-    </Button>
+    <>
+      <SidebarComponent
+        role="admin"
+        firstname={firstname}
+        lastname={lastname}
+        course={course}
+        isSmallScreen={isSmallScreen}
+        logOut={logOut}
+        group={group}
+      />
+    </>
   );
 }
